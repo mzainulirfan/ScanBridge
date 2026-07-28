366f23697be8d5b735bbf242925248ded9fefec0
mzainulirfan <mzainulirf@gmail.com>
2026-07-28 07:13:37 +0700

    feat: make mobile app installable as pwa

--- mobile/src/main.tsx
+++ mobile/src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    void navigator.serviceWorker.register("/sw.js", { scope: "/" });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
